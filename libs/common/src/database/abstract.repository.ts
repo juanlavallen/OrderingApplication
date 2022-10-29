6e12aeed6347d8c8504398363eb89d32c77e7276
Juan Lavallen <juanlavallen02@gmail.com>
2022-10-29 20:10:18 -0300

    first commit

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,9 @@
+export abstract class AbstractRepository {
+  //   constructor() {}
+  //   async create() {}
+  //   async findOne() {}
+  //   async findOneAndUpdate() {}
+  //   async upsert() {}
+  //   async find() {}
+  //   async startTransaction() {}
+}
