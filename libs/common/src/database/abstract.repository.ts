f25b8155562642306d8b241ba5ad92f6fef2b997
Juan Lavallen <juanlavallen02@gmail.com>
2022-10-29 20:40:47 -0300

    async method to find one

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -29,4 +29,15 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
       await createdDocument.save(options)
     ).toJSON()) as unknown as TDocument;
   }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
 }
