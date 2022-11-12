887c5713dd934d7d43c8b1bbe5ca2e1b3441a09d
juanlavallen <juanlavallen02@gmail.com>
2022-11-12 11:39:03 -0300

    validation pipe

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { OrdersModule } from './orders.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
