1525aff8efd26f7c62234d854fa1e71f109dbe7a
juanlavallen <juanlavallen02@gmail.com>
2022-11-12 11:40:21 -0300

    controller name

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateOrderRequest } from './dto';
 import { OrdersService } from './orders.service';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
